7f87360f09d08ce90f5e2465cf29e46a51b983b9
Jimin Seo <aa090402@naver.com>
2026-08-20 23:07:12 +0900

    feat: add analytics.google.com

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,34 +22,31 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
-  return (
-      <html
-          lang="ko"
-          className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      >
-      <body className="min-h-full flex flex-col">
-      {/* Google Tag Manager */}
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TG5GBRN4');
-          `}
-      </Script>
-      {/* Google Tag Manager (noscript) */}
-      <noscript>
-        <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TG5GBRN4"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
+    return (
+        <html
+            lang="ko"
+            className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+        >
+        <body className="min-h-full flex flex-col">
+        {/* Google Analytics */}
+        <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-43CS6N5W5V"
+            strategy="afterInteractive"
         />
-      </noscript>
 
-      {children}
-      </body>
-      </html>
-  );
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-43CS6N5W5V');
+          `}
+        </Script>
+
+        {children}
+        </body>
+        </html>
+    );
 }
