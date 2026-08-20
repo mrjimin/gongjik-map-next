e38a3b72945c3d35dae5c57db7022a11048e4f3d
Jimin Seo <aa090402@naver.com>
2026-08-20 20:16:27 +0900

    feat: add Google 애널리틱스

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,11 +23,33 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="ko"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+      <html
+          lang="ko"
+          className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+      <body className="min-h-full flex flex-col">
+      {/* Google Tag Manager */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TG5GBRN4');
+          `}
+      </Script>
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TG5GBRN4"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
+
+      {children}
+      </body>
+      </html>
   );
 }
